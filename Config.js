283cdc3432d1098e4b919edad388fe66ccdbfa6b
David Toli <59339822+davidtoli@users.noreply.github.com>
2024-07-09 01:19:45 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "254725458445"],
+global.ownername = process.env.OWNER_David Toli || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
@@ -12,9 +12,9 @@ global.location = "Indian"
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER Toli || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.packname = 'Sticker By'Toli
+global.author = process.env.AUTHOR || "💙Toli𓆩‎✘AʏᴜSʜ✘𓆪_✰",
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
